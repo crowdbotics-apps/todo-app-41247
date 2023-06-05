ca01a6f45c7d3bc308667accdd0badc311af7e86
Crowdbotics <team@crowdbotics.com>
2023-06-05 08:58:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "todo-app-41247",
+  url: "https://todo-app-41247.botics.co",
+  api: "https://todo-app-41247.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
